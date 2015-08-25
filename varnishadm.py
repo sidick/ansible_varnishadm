3b18a82ea70f90090f70a4b2241d60231f86209c
Simon Dick <simond@irrelevant.org>
2015-08-25 17:14:34 +0100

    Start to add name support

--- varnishadm.py
+++ varnishadm.py
@@ -36,6 +36,12 @@ options:
     default: null
     choices: ['auto', 'healthy', 'sick']
     aliases: []
+  name:
+    description:
+      - Connect to the instance of varnishd with this name
+    required: false
+    default: null
+    aliases: []
 '''
 
 EXAMPLES = '''
@@ -51,8 +57,13 @@ EXAMPLES = '''
 '''
 
 
-def get_state(module, secret, backend):
-    cmd = "varnishadm -S '%s' backend.list %s" % (secret, backend)
+def get_state(module, secret, backend, name):
+    args = ''
+
+    if name:
+      args += ' -n %s ' % name
+
+    cmd = "varnishadm -S '%s' %s backend.list %s" % (secret, args, backend)
 
     (rc, out, err) = module.run_command(cmd)
     if rc:
@@ -69,14 +80,19 @@ def get_state(module, secret, backend):
             return state
 
 
-def change_state(module, secret, state, backend):
+def change_state(module, secret, state, backend, name):
 
-    current_state = get_state(module, secret, backend)
+    args = ''
+
+    if name:
+      args += ' -n %s ' % name
+
+    current_state = get_state(module, secret, backend, name)
 
     if state == current_state:
         return module.exit_json(changed=False)
 
-    cmd = "varnishadm -S '%s' backend.set_health %s %s" % (secret, backend, state)
+    cmd = "varnishadm -S '%s' %s backend.set_health %s %s" % (secret, args, backend, state)
 
     (rc, out, err) = module.run_command(cmd)
 
@@ -93,6 +109,7 @@ def main():
             state=dict(required=False, choices=['sick', 'healthy', 'auto']),
             secret=dict(required=False, default='/etc/varnish/secret', type='str'),
             backend=dict(required=False, default=None, type='str'),
+            name=dict(required=False, default=None, type='str'),
         ),
         supports_check_mode=False,
     )
@@ -100,12 +117,13 @@ def main():
     state = module.params['state']
     secret = module.params['secret']
     backend = module.params['backend']
+    name = module.params['name']
 
     if (state):
         if (not backend):
             module.fail_json(msg="Need to specify backend with the state")
 
-        change_state(module, secret, state, backend)
+        change_state(module, secret, state, backend, name)
 
     return module.fail_json(msg="Unknown usage")
 
